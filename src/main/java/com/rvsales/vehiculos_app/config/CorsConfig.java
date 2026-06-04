4306615460e3d042ca5d08aecb0ddb39de136d91
melisb3@gmail.com <melisb3@gmail.com>
2026-06-04 11:56:43 +0200

    CORS troublehooting

--- src/main/java/com/rvsales/vehiculos_app/config/CorsConfig.java
+++ src/main/java/com/rvsales/vehiculos_app/config/CorsConfig.java
@@ -15,8 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:3000",
-                                "https://super-pasca-612521.netlify.app"
+                                "*"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
